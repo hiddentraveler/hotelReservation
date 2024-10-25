97ded2b71c599360b8c465da841636c61b62a0f1
hiddentraveler <github.cincholoipon@aleeas.com>
2024-10-25 11:41:19 +0530

    Setup fiber and air

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	app := fiber.New()
 	fmt.Println("mercii")
+	app.Get("/", handleGreet)
+	app.Listen(":5000")
+}
+
+func handleGreet(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"msg": "Hello World"})
 }

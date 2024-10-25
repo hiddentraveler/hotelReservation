96c81404940d96d78fb94b012b9ac4918b56a2c3
hiddentraveler <github.cincholoipon@aleeas.com>
2024-10-25 12:05:58 +0530

    started api versioning and made a commandline option to set port

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	listenAddr := flag.String("listenAddr", ":5000", "Listen address of api server")
 	app := fiber.New()
 	fmt.Println("mercii")
+	apiv1 := app.Group("/api/v1")
+
+	apiv1.Get("/users", handleUsers)
 	app.Get("/", handleGreet)
-	app.Listen(":5000")
+	app.Listen(*listenAddr)
+}
+
+func handleUsers(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"user": "Neo"})
 }
 
 func handleGreet(c *fiber.Ctx) error {
